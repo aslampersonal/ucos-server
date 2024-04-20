4163559f207ca426179f59cf557359e0cf7571e5
Aslam Muhammed <azlamazl.aa@gmail.com>
2024-04-20 11:37:57 +0530

    vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
 const app = express();
+const cors = require("cors");
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+dotenv.config();
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+mongoose
+  .connect("mongodb+srv://ecos:NBnXAf3aCZzPaD2x@cluster0.7ctkgki.mongodb.net/ucos?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch(() => {
+    console.log("database connection failed");
+  });
 
-module.exports = app;
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// for admin routes
+const adminRoute = require("../routes/adminRoutes");
+app.use("/api/admin", adminRoute);
+
+// for user route
+const userRoute = require("../routes/userRoutes");
+app.use("/api/users", userRoute);
+
+app.listen(3000, () => {
+  console.log("server connected at 3000");
+});
+
